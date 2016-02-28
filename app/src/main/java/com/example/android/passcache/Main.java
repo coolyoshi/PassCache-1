bebb8b2e939dac5478649c161e95a5f4a835bf47
hza47 <hza47@sfu.ca>
2016-02-28 09:19:31 -0800

    aa

--- app/src/main/java/com/example/android/passcache/Main.java
+++ app/src/main/java/com/example/android/passcache/Main.java
@@ -2,7 +2,11 @@ package com.example.android.passcache;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
 public class Main extends AppCompatActivity {
 
     @Override
@@ -10,4 +14,8 @@ public class Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+
 }
