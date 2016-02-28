181336c7d34529237a2f09191ab82211b522c025
coolyoshi <yoonchris@live.ca>
2016-02-27 22:29:22 -0800

    merging

--- app/src/main/java/com/example/android/passcache/Login_Activity.java
+++ app/src/main/java/com/example/android/passcache/Login_Activity.java
@@ -2,6 +2,8 @@ package com.example.android.passcache;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class Login_Activity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Login_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+    }
+
+    public void buttonLoginClick(View view) {
+        Intent intent = new Intent(Login_Activity.this, Main.class);
+        startActivity(intent);
     }
 }
