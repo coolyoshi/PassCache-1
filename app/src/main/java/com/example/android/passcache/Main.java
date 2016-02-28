c2c4afcf6c9037834ec195f8db3fc8331943c8e4
coolyoshi <yoonchris@live.ca>
2016-02-28 05:03:24 -0800

    merge

--- app/src/main/java/com/example/android/passcache/Main.java
+++ app/src/main/java/com/example/android/passcache/Main.java
@@ -4,6 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
+import java.lang.Math;
+import android.widget.Button;
+import android.util.Log;
+import java.lang.Thread;
+
+import java.util.Vector;
 
 public class Main extends AppCompatActivity {
 
@@ -11,6 +17,5 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        createButton();
     }
 }
