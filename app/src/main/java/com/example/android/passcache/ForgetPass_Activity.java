88752ca55ebb513fb18c1a1bf4a138ca54b585bf
Kyle Wang <kwa52@sfu.ca>
2016-02-27 17:31:49 -0800

    adding two other activities

--- app/src/main/java/com/example/android/passcache/ForgetPass_Activity.java
+++ app/src/main/java/com/example/android/passcache/ForgetPass_Activity.java
@@ -3,11 +3,11 @@ package com.example.android.passcache;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ForgetPass_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_forget_pass_);
     }
 }
